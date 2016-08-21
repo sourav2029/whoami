d17eb944aea1f1b20477c91c4a01cf47dcb25bcb
sourav <souravprem77@gmail.com>
2016-08-21 15:46:30 +0530

    added readme

--- readme.md
+++ readme.md
@@ -1,1 +1,25 @@
 THis is for the freecodecamp assignment https://www.freecodecamp.com/challenges/request-header-parser-microservice
+
+User Story:
+1)I can get the IP address, language and operating system for my browser.
+
+
+Example output:
+{"ipaddress":"111.91.227.2","language":"en-US","software":"Windows NT 6.2; Win64; x64"}
+
+1)git clone https://github.com/sourav2029/whoami
+
+2)move inside the directory
+
+3)node server.js
+
+Example url: if you want to run it locally on your system
+
+http://localhost:3000/api/whoami
+
+
+Example url if you want to test my aap
+
+https://tstamp-miniservice.herokuapp.com/dec 20, 2014
+
+https://tstamp-miniservice.herokuapp.com/1419013800000
--- server.js
+++ server.js
@@ -2,16 +2,21 @@ var express=require('express')
 var fs=require('fs')
 var port=(process.env.PORT || 3000)
 var app=express()
+app.get('/',function(req,res){
+    res.writeHead(200,{'content-type':'text/plain'})
+    var src=fs.createReadStream('readme.md');
+    src.pipe(res);
+})
 
-app.get('/'/*api/whoami'*/,function(req,res){
+app.get('/api/whoami',function(req,res){
   var userAgent=req.headers['user-agent'].split('(')[1];
   var os=userAgent.split(')')[0];
   console.log(os);
   var object={
-    "ipaddress":req.headers['x-forwarded-for'] ||
+    "ipaddress":req.ip/*req.headers['x-real-ip'] ||
      req.connection.remoteAddress ||
      req.socket.remoteAddress ||
-     req.connection.socket.remoteAddress,
+     req.connection.socket.remoteAddress*/,
     "language":req.headers['accept-language'].split(',')[0],
     "software":os
   }
